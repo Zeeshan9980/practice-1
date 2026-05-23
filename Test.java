7333fc771ea0da703dde11d9184761e69156fedc
zeeshan <zeeshan1890@gmail.com>
2026-05-24 00:16:09 +0530

    addition

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test{
+    public static void main(String[]args){
+        int a=12;
+        int b=8;
+        int sum=a+b;
+        System.out.println("Performing addition "+sum);
+    }
+}
